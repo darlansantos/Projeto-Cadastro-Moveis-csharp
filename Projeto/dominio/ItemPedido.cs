8cbfd85e39779ecfb542ed2c08a585c2da6f7fe5
Darlan Parreiras <darlanparreiras@gmail.com>
2019-04-11 19:49:24 -0300

    Projeto finalizado

---
+++ Projeto/dominio/ItemPedido.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Projeto.dominio
+{
+    class ItemPedido
+    {
+        public int Quantidade { get; set; }
+        public double PorcentagemDesconto { get; set; }
+        public Pedido Pedido { get; set; } // Associação entre os objetos
+        public Produto Produto { get; set; } // Associação entre os objetos
+
+        public ItemPedido(int quantidade, double porcentagemDesconto, Pedido pedido, Produto produto)
+        {
+            Quantidade = quantidade;
+            PorcentagemDesconto = porcentagemDesconto;
+            Pedido = pedido;
+            Produto = produto;
+        }
+
+        public double subTotal()
+        {
+            double desconto = Produto.Preco * PorcentagemDesconto / 100;
+            return (Produto.Preco - desconto) * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Produto.Descricao 
+                + ", Preço: " 
+                + Produto.Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", Qte: " 
+                + Quantidade 
+                + ", Desconto: " 
+                + PorcentagemDesconto.ToString("F2", CultureInfo.InvariantCulture) 
+                + "%, Subtotal: " 
+                + subTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projeto/dominio/Pedido.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Projeto.dominio
+{
+    class Pedido
+    {
+        public int Codigo { get; set; }
+        public DateTime Data { get; set; }
+        public List<ItemPedido> itens { get; set; } 
+
+        public Pedido(int codigo, int dia, int mes, int ano)
+        {
+            Codigo = codigo;
+            Data = new DateTime(ano, mes, dia);
+            itens = new List<ItemPedido>();
+        }
+
+        public double valorTotal()
+        {
+            double soma = 0.0;
+            for(int i = 0; i < itens.Count; i++)
+            {
+                soma = soma + itens[i].subTotal();
+            }
+            return soma;
+        }
+
+        public override string ToString()
+        {
+            string s = "Pedido: " + Codigo
+                + ", data: " + Data.Day + "/" + Data.Month + "/" + Data.Year + "\n"
+                + "Itens :\n";
+            for(int i = 0; i < itens.Count; i++)
+            {
+                s = s + itens[i] + "\n";
+            }
+                s = s + "Total do Pedido: " + valorTotal().ToString("F2", CultureInfo.InvariantCulture);
+                return s;
+        }
+    }
+}
